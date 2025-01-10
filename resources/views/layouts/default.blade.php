05708cb5ea6279cf5bcf30055a172648a063c00f
Lina-Null <lina0709521@icloud.com>
2025-01-10 07:27:37 +0000

    完成用户注册功能

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -20,6 +20,7 @@
   </nav> --}}
   <div class="container">
     <div class="offset-md-1 col-md-10">
+      @include('shared._messages')
       @yield('content')
       @include('layouts._footer')
     </div>
