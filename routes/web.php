c3de18e6c67d425406d8848de27abb6488c1888b
Lina-Null <lina0709521@icloud.com>
2025-01-15 09:09:53 +0000

    关注按钮

--- routes/web.php
+++ routes/web.php
@@ -51,6 +51,11 @@ Route::post('password/reset',  'PasswordController@reset')->name('password.updat
 
 // 微博
 Route::resource('statuses', 'StatusesController', ['only' => ['store', 'destroy']]);
+
 //关注列表 粉丝列表
 Route::get('/users/{user}/followings', 'UsersController@followings')->name('users.followings');
 Route::get('/users/{user}/followers', 'UsersController@followers')->name('users.followers');
+
+//关注用户 和 取消用户
+Route::post('/users/followers/{user}', 'FollowersController@store')->name('followers.store');
+Route::delete('/users/followers/{user}', 'FollowersController@destroy')->name('followers.destroy');
